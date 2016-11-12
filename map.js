695d6c99b880e5374568f53fd33064880fc092e7
disorn <disornkwunchaithanya@hotmail.com>
2016-11-12 11:31:25 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,6 +2,19 @@ function fixedPoints(){
   
 }
 
+var map;
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(39.8282, -98.5795),
+    zoom:4,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+initialize();
+
 function myMap(){
   var mapCanvas = document.getElementById("map");
   //NE HQ Buffalo:
